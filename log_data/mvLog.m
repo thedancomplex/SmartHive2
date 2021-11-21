3bd3130fb5d0e4a5e90bd489bf60a026feeef310
Daniel M. Lofaro <lofaro@umd.edu>
2021-11-20 21:22:04 -0500

    added plot, need to get the legeond to work better

--- log_data/mvLog.m
+++ log_data/mvLog.m
@@ -2,14 +2,18 @@ close all
 clear all
 a=dlmread('temp-test-20211113_1649.log',',');
 
-v1 = []
-v2 = []
-v3 = []
-v4 = []
-v1i = 1
-v2i = 1
-v3i = 1
-v4i = 1
+v1 = [];
+v2 = [];
+v3 = [];
+v4 = [];
+v1i = 1;
+v2i = 1;
+v3i = 1;
+v4i = 1;
+
+tt = a(1,1);
+kk = 60*60*24;
+sh = 1649/2400.0;
 
 for i=1:length(a)
   ii = a(i,2);
@@ -29,25 +33,50 @@ for i=1:length(a)
 end
 
 figure
-hold on
-for i=1:6
-  plot(v1(:,1), v1(:,2+i))
-end
 
-figure
-hold on
-for i=1:6
-  plot(v2(:,1), v2(:,2+i))
-end
 
-figure
-hold on
 for i=1:6
-  plot(v3(:,1), v3(:,2+i))
+  subplot(4,1,1)
+  hold on
+  x = ((v1(:,1)-tt)/kk+sh);
+  plot(x, v1(:,2+i))
+  xticks([0:0.5:ceil(max(x))]);
+  title("Frame 1 (No Paint)")
 end
+%lgd = legend({'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6'},'FontSize',8);
+%set(gca,'XTick',[]);
 
-figure
-hold on
 for i=1:6
-  plot(v4(:,1), v4(:,2+i))
+  subplot(4,1,2)
+  hold on
+  x = ((v2(:,1)-tt)/kk+sh);
+  plot(x, v2(:,2+i))
+  xticks([0:0.5:ceil(max(x))]);
+  title("Frame 2 (No Paint)")
 end
+%lgd = legend({'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6'},'FontSize',10);
+%set(gca,'XTick',[]);
+
+for i=1:6
+  subplot(4,1,3)
+  hold on
+  x = ((v3(:,1)-tt)/kk+sh);
+  plot(x, v3(:,2+i))
+  xticks([0:0.5:ceil(max(x))]);
+  title("Frame 3 (With Paint)")
+end
+%lgd = legend({'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6'},'FontSize',10);
+%set(gca,'XTick',[]);
+
+for i=1:6
+  subplot(4,1,4)
+  hold on
+  x = ((v4(:,1)-tt)/kk+sh);
+  plot(x, v4(:,2+i))
+  xticks([0:0.5:ceil(max(x))]);
+  title("Frame 4 (With Paint)")
+end
+%lgd = legend({'Sensor 1', 'Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6'},'FontSize',10);
+%suptitle("Temp Test 2021-11-13 to 2021-11-20")
+
+print -color -depsc plots_test_2021-11-13_to_2021-11-20.eps
